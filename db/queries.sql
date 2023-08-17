6acb5dbad48b5b4aa997a151e1d152c704a8fe31
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-08-17 12:06:34 -0600

    added all sql files needed for the employee database

---
+++ db/queries.sql
@@ -0,0 +1,46 @@
+-- Show only department table
+SELECT *
+FROM department;
+
+-- Show only role table
+SELECT *
+FROM role;
+
+-- Show only employee table
+SELECT *
+FROM employee;
+
+-- Join department table to role table
+SELECT role.*, department.name AS department_name
+FROM role
+JOIN department ON role.department_id = department.id;
+
+-- Join role table to employee table
+SELECT employee.id AS employee_id,
+       employee.first_name,
+       employee.last_name,
+       role.title AS role_title,
+       role.salary AS role_salary,
+       employee.manager_id
+FROM employee
+JOIN role ON employee.role_id = role.id;
+
+-- View All Employees. Join role table to employee table together with department table. Change department.name to "department" and employee.manager_id to "manager"
+SELECT employee.id,
+       employee.first_name,
+       employee.last_name,
+       role.title,
+       department.name AS department,
+       role.salary,
+       employee.manager_id AS manager
+FROM employee
+JOIN role ON employee.role_id = role.id
+JOIN department ON role.department_id = department.id;
+
+-- View All Roles. Joins department table to role table and changes depart.name to "department"
+SELECT role.id,
+       role.title,
+       department.name AS department,
+       role.salary
+FROM role
+JOIN department ON role.department_id = department.id;
